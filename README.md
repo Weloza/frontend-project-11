6a375e5cf2eeb104c46335bbeab47b3ac129649d
Weloza <danil557@icloud.com>
2024-10-23 21:36:44 +0400

    add initial environment setup, initial site version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Weloza/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Weloza/frontend-project-11/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/75bb320ec56c594ca824/maintainability)](https://codeclimate.com/github/Weloza/frontend-project-11/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+    entry: './src/index.js',
+    mode: process.env.NODE_ENV || 'development',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    devServer: {
+      open: true,
+      host: 'localhost',
+      client: {
+        overlay: {
+          warnings: false,
+          errors: true,
+        },
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+    ],
+    module: {
+        rules: [
+          { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+          {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+          },
+        ],
+      },
+};
